7cc302773bd96e38bbd7a981398fe2fe9605ca44
coderamritanshu <77872905+coderamritanshu@users.noreply.github.com>
2021-05-03 08:01:20 +0530

    Create calling.sh

---
+++ calling.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+apt update && apt upgrade -y
+pkg install termux-api
+pkg install figlet
+figlet CALLER 
+figlet MACHINE
+read -p "Phone Number:" num
+
+if [$num -gt 1000000000 && $num -lt 9999999999];  
+then  
+  echo "termux-telephony-call" $num 
+else  
+  echo "Please enter correct number"
+  
+fi  
+
+
+echo "Thanks for Using Our Software."
