7544dad268cb5b4ec28eec132d678b13f0fa88e6
coderamritanshu <77872905+coderamritanshu@users.noreply.github.com>
2021-05-03 08:39:37 +0530

    Update calling.sh

--- calling.sh
+++ calling.sh
@@ -5,15 +5,8 @@ pkg install termux-api
 pkg install figlet
 figlet CALLER 
 figlet MACHINE
-read -p "Phone Number:" num
-
-if [$num -gt 1000000000 && $num -lt 9999999999];  
-then  
+read -p "Phone Number:" num  
   termux-telephony-call $num 
-else  
-  echo "Please enter correct number"
-  
-fi  
 
 
 echo "Thanks for Using Our Software."
